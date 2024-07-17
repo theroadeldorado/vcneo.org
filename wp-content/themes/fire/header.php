9a63c0681334093e7e4512775f52eaf0ba4d5427
John Young <theroad2eldorado@gmail.com>
2024-07-16 21:57:59 -0400

    updates logo

--- wp-content/themes/fire/header.php
+++ wp-content/themes/fire/header.php
@@ -57,12 +57,12 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <nav>
       <div class="container mx-auto px-4">
         <div class="flex h-24 items-center">
-          <a class="block w-8" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+          <a class="block w-28 lg:w-44" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
             <?php new Fire_SVG('logo'); ?>
             <span class="sr-only"><?php bloginfo( 'name' ); ?></span>
           </a>
           <button x-on:click="mobileNavOpen = !mobileNavOpen" class="lg:hidden py-1 ml-auto">
-            <svg width="44" height="16" viewbox="0 0 44 16" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="44" height="2" rx="1" fill="black"></rect><rect y="14" width="44" height="2" rx="1" fill="black"></rect></svg>
+            <svg width="44" height="16" viewbox="0 0 44 16" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="44" height="2" rx="1" fill="white"></rect><rect y="14" width="44" height="2" rx="1" fill="white"></rect></svg>
           </button>
           <?php
             wp_nav_menu([
