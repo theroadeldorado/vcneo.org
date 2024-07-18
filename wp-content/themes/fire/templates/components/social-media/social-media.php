3639214777b0b47d49713532c8f7fa8ed7f9cae5
John Young <theroad2eldorado@gmail.com>
2024-07-17 22:13:11 -0400

    updates link

--- wp-content/themes/fire/templates/components/social-media/social-media.php
+++ wp-content/themes/fire/templates/components/social-media/social-media.php
@@ -18,12 +18,14 @@
       <?php if($social_links) :?>
         <div class="flex flex-wrap items-center justify-center mb-10 gap-6">
           <?php  foreach ($social_links as $platform => $link):?>
-            <a class="block text-white no-underline hover:scale-110 duration-200 ease-in-out" target="_blank" href="<?php echo $link;?>">
-              <span class="inline-flex p-2 w-16 h-16 md:w-18 md:h-18 items-center justify-center bg-white rounded-2xl <?php echo $bg_style === 'blue' ? 'text-blue-500' : 'text-green-500';?>">
-                <?php new Fire_SVG('icon--social-' . $platform); ?>
-                <span class="sr-only"><?php echo $platform; ?></span>
-              </span>
-            </a>
+            <?php if($link):?>
+              <a class="block text-white no-underline hover:scale-110 duration-200 ease-in-out" target="_blank" href="<?php echo $link;?>">
+                <span class="inline-flex p-2 w-16 h-16 md:w-18 md:h-18 items-center justify-center bg-white rounded-2xl <?php echo $bg_style === 'blue' ? 'text-blue-500' : 'text-green-500';?>">
+                  <?php new Fire_SVG('icon--social-' . $platform); ?>
+                  <span class="sr-only"><?php echo $platform; ?></span>
+                </span>
+              </a>
+            <?php endif; ?>
           <?php endforeach; ?>
         </div>
       <?php endif; ?>
