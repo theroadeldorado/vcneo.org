b8bce0f5cb8796b3e1b35c4e7428b6f324097a2a
John Young <theroad2eldorado@gmail.com>
2024-07-17 22:03:28 -0400

    updates footer

--- wp-content/themes/fire/footer.php
+++ wp-content/themes/fire/footer.php
@@ -9,6 +9,10 @@
  * @package Fire
  */
 
+  $address = get_field('address', 'site_settings');
+  $phone_number = get_field('phone_number', 'site_settings');
+  $social_links = get_field('social_links', 'site_settings');
+
 ?>
   <footer class="py-12 md:py-20 bg-black text-white">
     <div class="container mx-auto px-4">
@@ -18,27 +22,47 @@
             <?php new Fire_SVG('logo'); ?>
             <span class="sr-only"><?php bloginfo( 'name' ); ?></span>
           </a>
-        </div>
-        <div class="w-full lg:w-8/12 xl:w-7/12 px-4">
-          <?php
-          wp_nav_menu(
-            array(
-              'container'       => false,
-              'depth'           => 1,
-              'theme_location'  => 'footer',
-              'menu_class'      => 'menu_class [&>ul]:flex [&>ul]:flex-wrap [&>ul]:-mb-2 [&>ul]:items-center [&>ul]:lg:justify-end [&>ul]:gap-14',
-              'link_class'      => 'inline-block text-white hover:text-coolGray-400',
-              'item_class'      => 'mb-2 mr-14',
-            )
-          );
-        ?>
+          <?php if($address):?>
+            <p class="text-white text-2xl mt-6 block"><?php echo $address;?></p>
+          <?php endif; ?>
+          <?php if($phone_number):?>
+            <a class="link !text-white mt-4 block" href="tel:<?php echo $phone_number;?>"><?php echo $phone_number;?></a>
+          <?php endif; ?>
 
         </div>
-      </div>
-      <div class="flex flex-wrap -mx-4 items-center">
-        <div class="w-full sm:w-1/2 px-4 mb-6 sm:mb-0">
-          <span class="text-sm text-coolGray-600"> <?php echo sprintf('© %s %s', date('Y'), get_bloginfo('name')); ?></span>
+        <div class="w-full lg:w-8/12 xl:w-7/12 px-4">
+          <div>
+            <?php
+              wp_nav_menu(
+                array(
+                  'container'       => false,
+                  'depth'           => 1,
+                  'theme_location'  => 'footer',
+                  'menu_class'      => 'menu_class flex flex-wrap gap-x-6 lg:gap-x-14 mb-8 items-center lg:justify-end gap-14 w-full',
+                  'link_class'      => 'inline-block text-white hover:text-coolGray-400',
+                  'item_class'      => '',
+                )
+              );
+            ?>
+          </div>
         </div>
+      </div>
+      <div class="flex flex-wrap -mx-4 items-center justify-between">
+        <div class="w-full sm:w-1/2 px-4 mb-6">
+          <span class="text-sm text-white"> <?php echo sprintf('© %s %s', date('Y'), get_bloginfo('name')); ?></span>
+        </div>
+         <?php if($social_links) :?>
+            <div class="flex flex-wrap items-center justify-end mb-10 gap-6 px-4">
+              <?php  foreach ($social_links as $platform => $link):?>
+                <a class="block text-white no-underline hover:scale-110 duration-200 ease-in-out" target="_blank" href="<?php echo $link;?>">
+                  <span class="inline-flex p-2 w-10 h-10 items-center justify-center bg-white rounded-md text-black">
+                    <?php new Fire_SVG('icon--social-' . $platform); ?>
+                    <span class="sr-only"><?php echo $platform; ?></span>
+                  </span>
+                </a>
+              <?php endforeach; ?>
+            </div>
+          <?php endif; ?>
       </div>
     </div>
   </footer>
